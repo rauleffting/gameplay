453f87dccf5d11436bdb22f1551fb7b1c7745de7
Raul Francisco Effting <rauleffting94@gmail.com>
2022-12-13 19:19:02 -0300

    add linear gradient

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
+import { useState, useCallback, useEffect, } from 'react';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
-import AppLoading from 'expo-app-loading';
 
 import { SignIn } from './src/screens/SignIn';
+import { Background } from './src/components/Background';
 
 export default function App(){
   const [fontsLoaded] = useFonts({
@@ -13,18 +14,19 @@ export default function App(){
     Rajdhani_700Bold
   });
 
-  if(!fontsLoaded) {
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return null;
   }
 
   return(
-    <>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
-    <SignIn />
-    </>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </Background>
+
   );
 }
